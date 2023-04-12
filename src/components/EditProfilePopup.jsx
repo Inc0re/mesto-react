8309ea770bb5b0ced0ce3850622497beb69cd24a
Daniil Borovov <superskillbro@gmail.com>
2023-04-12 16:39:53 +0600

    use state and context to render EditProfilePopup

--- src/components/EditProfilePopup.jsx
+++ src/components/EditProfilePopup.jsx
@@ -1,39 +1,64 @@
+import React from 'react';
 import PopupWithForm from './PopupWithForm';
+import { CurrentUserContext } from '../contexts/CurrentUserContext';
 
-function EditProfilePopup({isOpen, onClose}) {
+function EditProfilePopup({ isOpen, onClose }) {
+  const [name, setName] = React.useState('');
+  const [description, setDescription] = React.useState('');
+  const currentUser = React.useContext(CurrentUserContext);
+
+  React.useEffect(() => {
+    if (currentUser) {
+      setName(currentUser.name);
+      setDescription(currentUser.about);
+    }
+  }, [currentUser]);
+
+  function handleNameChange(e) {
+    setName(e.target.value);
+  }
+
+  function handleDescriptionChange(e) {
+    setDescription(e.target.value);
+  }
+
   return (
-          <PopupWithForm
-        name='edit-profile'
-        title='Редактировать профиль'
-        isOpen={isOpen}
-        onClose={onClose}
-        buttonText='Сохранить'
-      >
-        <div className='edit-form__wrapper'>
-          <input
-            className='edit-form__field'
-            name='name'
-            type='text'
-            placeholder='Имя'
-            required
-            minLength='2'
-            maxLength='40'
-          />
-          <span className='edit-form__error' />
-        </div>
-        <div className='edit-form__wrapper'>
-          <input
-            className='edit-form__field'
-            name='job'
-            type='text'
-            placeholder='Место работы'
-            required
-            minLength='2'
-            maxLength='200'
-          />
-          <span className='edit-form__error' />
-        </div>
-      </PopupWithForm>
+    <PopupWithForm
+      name='edit-profile'
+      title='Редактировать профиль'
+      isOpen={isOpen}
+      onClose={onClose}
+      buttonText='Сохранить'
+    >
+      <div className='edit-form__wrapper'>
+        <input
+          className='edit-form__field'
+          name='name'
+          type='text'
+          placeholder='Имя'
+          required
+          minLength='2'
+          maxLength='40'
+          value={name}
+          onChange={handleNameChange}
+        />
+        <span className='edit-form__error' />
+      </div>
+      <div className='edit-form__wrapper'>
+        <input
+          className='edit-form__field'
+          name='job'
+          type='text'
+          placeholder='Место работы'
+          required
+          minLength='2'
+          maxLength='200'
+          value={description}
+          onChange={handleDescriptionChange}
+        />
+        <span className='edit-form__error' />
+      </div>
+    </PopupWithForm>
   );
 }
 
